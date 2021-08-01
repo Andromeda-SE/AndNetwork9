c339e66b799cd37339e822662e3fee483675fa58
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2021-08-01 19:34:41 +0300

    Add repo creating

---
+++ AndNetwork9/Client/Pages/Repos/NewRepo.razor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using AndNetwork9.Client.Services;
+using AndNetwork9.Shared.Enums;
+using AndNetwork9.Shared.Utility;
+using Microsoft.AspNetCore.Components;
+
+namespace AndNetwork9.Client.Pages.Repos
+{
+    public partial class NewRepo
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string Name { get; set; }
+        [Parameter]
+        public RepoType Type { get; set; }
+        [Parameter]
+        public AccessRule ReadRule { get; set; } = new ()
+        {
+            Directions = Enum.GetValues<Direction>().Where(x => x > Direction.None).ToArray(),
+            MinRank = Rank.Neophyte,
+            Squad = null,
+            SquadId = null,
+        };
+        [Parameter]
+        public AccessRule WriteRule { get; set; } = new ()
+        {
+            Directions = Enum.GetValues<Direction>().Where(x => x > Direction.None).ToArray(),
+            MinRank = Rank.Advisor,
+            Squad = null,
+            SquadId = null,
+        };
+
+        private async System.Threading.Tasks.Task CreateRepo()
+        {
+            HttpResponseMessage readResponse = await Client.PostAsJsonAsync("api/AccessRule", ReadRule);
+            HttpResponseMessage writeResponse = await Client.PostAsJsonAsync("api/AccessRule", WriteRule);
+            AccessRule readRule = await readResponse.Content.ReadFromJsonAsync<AccessRule>();
+            AccessRule writeRule = await writeResponse.Content.ReadFromJsonAsync<AccessRule>();
+
+            HttpResponseMessage repoResponse = await Client.PostAsJsonAsync("api/repo", new AndNetwork9.Shared.Storage.Repo()
+            {
+                ReadRuleId = readRule.Id,
+                ReadRule = readRule,
+                WriteRuleId = writeRule.Id,
+                WriteRule = writeRule,
+                CreatorId = AuthStateProvider.CurrentMember.Id,
+                Creator = AuthStateProvider.CurrentMember,
+                Name = Name,
+                Type = Type,
+                RepoName = Name,
+            });
+            if (repoResponse.IsSuccessStatusCode)
+            {
+                AndNetwork9.Shared.Storage.Repo repo = await repoResponse.Content.ReadFromJsonAsync<AndNetwork9.Shared.Storage.Repo>();
+                NavigationManager.NavigateTo($"repo/{repo.Id}");
+            }
+        }
+    }
+}
--- AndNetwork9/Storage/Listeners/NewRepoListener.cs
+++ AndNetwork9/Storage/Listeners/NewRepoListener.cs
@@ -6,36 +6,51 @@ using AndNetwork9.Shared.Backend;
 using AndNetwork9.Shared.Backend.Rabbit;
 using AndNetwork9.Shared.Backend.Senders.Storage;
 using AndNetwork9.Shared.Storage;
+using AndNetwork9.Shared.Utility;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.DependencyInjection;
 using RabbitMQ.Client;
 
 namespace AndNetwork9.Storage.Listeners
 {
     public class NewRepoListener : BaseRabbitListenerWithResponse<Repo, Repo>
     {
-        private readonly ClanDataContext _data;
+        private readonly IServiceScopeFactory _scopeFactory;
         private readonly RepoManager _repoManager;
 
-        public NewRepoListener(IConnection connection, ClanDataContext data) : base(connection,
+        public NewRepoListener(IConnection connection, IServiceScopeFactory scopeFactory) : base(connection,
             NewRepoSender.QUEUE_NAME)
         {
-            _data = data;
+            _scopeFactory = scopeFactory;
             _repoManager = new();
         }
 
         protected override async Task<Repo> GetResponseAsync(Repo request)
         {
-            if (_data.Repos.Any(x => x.RepoName == request.RepoName))
-                throw new ArgumentException(null, nameof(request));
+            await using AsyncServiceScope scope = _scopeFactory.CreateAsyncScope();
+            ClanDataContext? data = (ClanDataContext?)scope.ServiceProvider.GetService(typeof(ClanDataContext));
+            if (data is null) throw new ApplicationException();
+            if (data.Repos.Any(x => x.RepoName == request.RepoName))
+                    throw new ArgumentException(null, nameof(request));
 
             request.RepoName = Guid.NewGuid().ToString("N");
 
             _repoManager.CreateRepo(request);
-            EntityEntry<Repo> result = await _data.Repos.AddAsync(request with
-            {
-                Nodes = new List<RepoNode>(),
+            EntityEntry<Comment>? comment = data.Comments.Add(new Comment()
+            { 
+                AuthorId = request.CreatorId, 
+                CreateTime = DateTime.UtcNow, 
+                LastEditTime = null, 
+                Text = string.Empty,
             });
-            await _data.SaveChangesAsync();
+            await data.SaveChangesAsync();
+            request.CommentId = comment.Entity.Id;
+            EntityEntry<Repo> result = await data.Repos.AddAsync(request with
+            { 
+                Nodes = new List<RepoNode>(), 
+                Creator = null
+            });
+            await data.SaveChangesAsync();
             return result.Entity;
         }
     }
