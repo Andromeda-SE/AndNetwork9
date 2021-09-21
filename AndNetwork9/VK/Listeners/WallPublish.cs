ba34299849a850f4b2e8ed390998394507c57835
sergdrakula2017 <34153361+sergdrakula2017@users.noreply.github.com>
2021-09-21 23:22:24 +0300

    Реализован сервис VK

---
+++ AndNetwork9/VK/Listeners/WallPublish.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using AndNetwork9.Shared.Backend.Rabbit;
+using AndNetwork9.Shared.Backend.Senders.VK;
+using AndNetwork9.VK;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using VkNet;
+using VkNet.Model.Attachments;
+using VkNet.Model.RequestParams;
+
+    public class WallPublish : BaseRabbitListenerWithoutResponse<string>
+{
+    public override Task Run(string request)
+    {
+
+        var post = vkapi.Wall.Post(new WallPostParams
+        {
+            OwnerId = -207090758,
+            FromGroup = true,
+            Message = request,
+            CloseComments = false,
+
+
+        });
+        return Task.CompletedTask;
+    }
+
+    public WallPublish(IConnection connection,  ILogger<BaseRabbitListenerWithoutResponse<string>> logger) : base(connection, WallPublishSender.QUEUE_NAME, logger)
+    {
+    }
+
+    private readonly VkApi vkapi;
+}
---
+++ AndNetwork9/VK/VKBot.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using VkNet;
+using VkNet.Model;
+
+namespace AndNetwork9.VK
+{
+    public class VkBot: VkApi,IHostedService
+    {
+        private readonly string _authorizationToken;
+      
+        public VkBot(IConfiguration configuration)
+        {
+             _authorizationToken = configuration["Vk_Token"];
+        }
+           
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+
+           await AuthorizeAsync(new ApiAuthParams()
+            {
+                AccessToken = _authorizationToken
+            });
+
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+           await LogOutAsync();
+        }
+    }
+}
