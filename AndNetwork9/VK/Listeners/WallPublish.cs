06ae18d2f00a3fe84f0eacd89e9f1765d4d12f04
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2021-09-22 21:12:32 +0300

    Add GroupId to config

--- AndNetwork9/VK/Listeners/WallPublish.cs
+++ AndNetwork9/VK/Listeners/WallPublish.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using AndNetwork9.Shared.Backend.Rabbit;
 using AndNetwork9.Shared.Backend.Senders.VK;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using VkNet;
@@ -9,16 +11,23 @@ namespace AndNetwork9.VK.Listeners
 {
     public class WallPublish : BaseRabbitListenerWithoutResponse<string>
     {
+        private readonly long _groupId;
         private readonly VkApi _vkApi;
 
-        public WallPublish(IConnection connection, ILogger<BaseRabbitListenerWithoutResponse<string>> logger, VkApi vkApi) :
-            base(connection, WallPublishSender.QUEUE_NAME, logger) => this._vkApi = vkApi;
+        public WallPublish(IConnection connection, ILogger<BaseRabbitListenerWithoutResponse<string>> logger,
+            IConfiguration configuration, VkApi vkApi) :
+            base(connection, WallPublishSender.QUEUE_NAME, logger)
+        {
+            _vkApi = vkApi;
+            _groupId = -long.Parse(configuration["Vk_GroupId"]);
+            if (_groupId > 0) throw new ArgumentException();
+        }
 
         public override async Task Run(string request)
         {
             await _vkApi.Wall.PostAsync(new()
             {
-                OwnerId = -207090758,
+                OwnerId = _groupId,
                 FromGroup = true,
                 Message = request,
                 CloseComments = false,
