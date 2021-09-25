a52e96fb8c455778a31f05850ef579535ed25653
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2021-09-25 03:13:22 +0300

    Update DateOnly converter

--- AndNetwork9/Shared/Converters/DateOnlyConverter.cs
+++ AndNetwork9/Shared/Converters/DateOnlyConverter.cs
@@ -7,11 +7,9 @@ namespace AndNetwork9.Shared.Converters
     public class DateOnlyConverter : JsonConverter<DateOnly>
     {
         public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
-            DateOnly.FromDayNumber(reader.GetInt32());
+            DateOnly.Parse(reader.GetString()!);
 
-        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
-        {
-            writer.WriteNumberValue(value.DayNumber);
-        }
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options) => 
+            writer.WriteStringValue(value.ToString("O"));
     }
 }
