bfd6adf6b15570cd653a99654406535c846f3b3b
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2021-09-26 20:49:55 +0300

    Fix buld error

--- AndNetwork9/Shared.Backend/Senders/VK/ResolveVkUrlSender.cs
+++ AndNetwork9/Shared.Backend/Senders/VK/ResolveVkUrlSender.cs
@@ -6,7 +6,7 @@ namespace AndNetwork9.Shared.Backend.Senders.VK
     public class ResolveVkUrlSender : BaseRabbitSenderWithResponse<string, long?>
     {
         public const string QUEUE_NAME = "Vk.ResolveVkUrl";
-        protected ResolveVkUrlSender(IConnection connection, ILogger<BaseRabbitSenderWithoutResponse<string>> logger) : base(connection, QUEUE_NAME, logger)
+        protected ResolveVkUrlSender(IConnection connection, ILogger<BaseRabbitSenderWithResponse<string, long?>> logger) : base(connection, QUEUE_NAME, logger)
         {
         }
     }
