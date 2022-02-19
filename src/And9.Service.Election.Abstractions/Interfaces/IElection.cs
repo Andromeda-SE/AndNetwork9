d4821c9c57957617252f2a5f9ea29b97a8e4fcc1
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2022-02-19 15:10:03 +0300

    Начальная реализация And9.Service.Election.Abstractions

---
+++ src/And9.Service.Election.Abstractions/Interfaces/IElection.cs
@@ -0,0 +1,16 @@
+﻿using And9.Lib.Models.Abstractions.Interfaces;
+using And9.Service.Core.Abstractions.Enums;
+using And9.Service.Election.Abstractions.Enums;
+
+namespace And9.Service.Election.Abstractions.Interfaces;
+
+public interface IElection : IId
+{
+    short ElectionId { get; }
+    int IId.Id => ElectionId << 16 + (int)Direction;
+
+    DateOnly AdvisorsStartDate { get; }
+    Direction Direction { get; }
+    ElectionStatus Status { get; }
+    int AgainstAllVotes { get; }
+}
---
+++ src/And9.Service.Election.Abstractions/Interfaces/IElectionVote.cs
@@ -0,0 +1,13 @@
+﻿using And9.Lib.Models.Abstractions.Interfaces;
+using And9.Service.Core.Abstractions.Enums;
+
+namespace And9.Service.Election.Abstractions.Interfaces;
+
+public interface IElectionVote : IId
+{
+    short ElectionId { get; }
+    Direction Direction { get; }
+    int MemberId { get; }
+    bool? Voted { get; }
+    int Votes { get; }
+}
