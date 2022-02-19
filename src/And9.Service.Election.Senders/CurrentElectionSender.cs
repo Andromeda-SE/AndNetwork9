83d4dbffc27eb0995a039ae04f6e4bde0df7ff66
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2022-02-19 16:45:06 +0300

    Service.Elections: Добавлен проект отправителей, определены отправители

---
+++ src/And9.Service.Election.Senders/CurrentElectionSender.cs
@@ -0,0 +1,11 @@
+﻿using And9.Lib.Broker;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace And9.Service.Election.Senders;
+
+public class CurrentElectionSender : BaseRabbitSenderWithResponse<int, Abstractions.Models.Election>
+{
+    public const string QUEUE_NAME = "And9.Service.Election.CurrentElection";
+    protected CurrentElectionSender(IConnection connection, ILogger<CurrentElectionSender> logger) : base(connection, QUEUE_NAME, logger) { }
+}
---
+++ src/And9.Service.Election.Senders/VoteSender.cs
@@ -0,0 +1,11 @@
+﻿using And9.Lib.Broker;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace And9.Service.Election.Senders;
+
+public class VoteSender : BaseRabbitSenderWithResponse<int, bool>
+{
+    public const string QUEUE_NAME = "And9.Service.Election.Vote";
+    public VoteSender(IConnection connection, ILogger<VoteSender> logger) : base(connection, QUEUE_NAME, logger) { }
+}
